29f423255eb362d327266e0a0672a48230f0e89f
taniya-cyber <taniyaarora809@gmail.com>
2026-07-12 21:06:19 +0530

    Added Min Heap Priprity Queue

--- STANDARD_TEMPLATE_LIBRARY/containers_stl/priority_queue.cpp
+++ STANDARD_TEMPLATE_LIBRARY/containers_stl/priority_queue.cpp
@@ -6,34 +6,40 @@
 
 int main(){
 
-    //Creation:
-    std::priority_queue<int> pq;
-    //when we create priority queue, by default, max - heap is created inside the memory. and maximum - valued element gets the priority.
+    // //Creation:
+    // std::priority_queue<int> pq;
+    // //when we create priority queue, by default, max - heap is created inside the memory. and maximum - valued element gets the priority.
 
-    //Inserting elements:
-    pq.push(12);
-    //12
-    pq.push(123);
-    // 123 -> 12
-    pq.push(8);
-    // 123 -> 12 -> 8 are rearranged.
+    // //Inserting elements:
+    // pq.push(12);
+    // //12
+    // pq.push(123);
+    // // 123 -> 12
+    // pq.push(8);
+    // // 123 -> 12 -> 8 are rearranged.
 
-    //top() is a function accessing the highest priority value inside priority queue.
-    std::cout<< pq.top() << std::endl;
+    // //top() is a function accessing the highest priority value inside priority queue.
+    // std::cout<< pq.top() << std::endl;
 
-    //Removal:
-    pq.pop(); //Highest priority element is removed or we can say element at the top.
+    // //Removal:
+    // pq.pop(); //Highest priority element is removed or we can say element at the top.
 
-    //It also does not have clear()
+    // //It also does not have clear()
 
-    //we can check if its empty by: if(pq.empty() == true) {.....}
+    // //we can check if its empty by: if(pq.empty() == true) {.....}
 
+    //Creation of Min - Heap:
+    std::priority_queue<int, std::vector<int>, std::greater<int> > pqm;
+   //<type_of_queue, type_of_container, greater_int>
+    pqm.push(10);
+    //10
+    pqm.push(230);
+    //10 -> 230
+    pqm.push(87);
+    //10 -> 87 -> 230
     
-
-
-
-
-
+    std::cout<< "Top of Min - heap queue:  " << pqm.top() << std::endl;
+    pqm.pop(); //removes top; 100.
 
     return 0;
 }
