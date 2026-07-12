babcdb7c625b9aa687fed744988d58719124351b
taniya-cyber <taniyaarora809@gmail.com>
2026-07-12 21:35:34 +0530

    Added Unordered Map Insertion Techniques

--- STANDARD_TEMPLATE_LIBRARY/containers_stl/map.cpp
+++ STANDARD_TEMPLATE_LIBRARY/containers_stl/map.cpp
@@ -1,4 +1,7 @@
 #include<iostream>
+#include<map>
+#include<unordered_map>
+// #include<ordered_map> does not exist.
 
 /* ->Map: A data structure, Just a table (Entries); Collection of Key - Value pairs. 
    -> Allows Efficient Retrieval and Modifications of values based on their keys
@@ -11,18 +14,19 @@
    -> Ordered - Not in random order.
    -> Unordered - Based on Array DS/ Hash table, In random order
 */
-int main(){
+int main(){ 
+    //Creation of Unordered Map: std::type_of_map<type_of_key, type_of_value> name_of_map;
+    std::unordered_map<std::string, std::string> table;
 
+    //Insertion in Unordered map: two ways
+    table.insert(std::make_pair("en", "England")); //"key", "value"
+    table["in"] = "India"; 
 
-
-
-
-
-
-
-
-
-
+    //3rd way:
+    std::pair<std::string, std::string> p;
+    p.first = "br";
+    p.second = "Brazil";
+    table.insert(p);
 
     return 0;
 }
