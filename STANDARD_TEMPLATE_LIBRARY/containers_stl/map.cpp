248a6eeedeb049489e837b177740a0905fa7f74d
taniya-cyber <taniyaarora809@gmail.com>
2026-07-14 16:30:50 +0530

    Added Find Function of Unordered Map

--- STANDARD_TEMPLATE_LIBRARY/containers_stl/map.cpp
+++ STANDARD_TEMPLATE_LIBRARY/containers_stl/map.cpp
@@ -33,23 +33,43 @@ int main(){
     // table.clear();
     // std::cout<< table.size() <<std::endl; //Now size = 0
 
-    //Acessing:
-    std::cout<< table.at("in") <<std::endl; // at("Key_name") returns what is present in that key.
-    //Updating value using at:
-    table.at("in") = "India2";
-    std::cout<< table.at("in") <<std::endl;
-    //Updating using []:
-    table["in"] = "India3";
-    std::cout<< table.at("in") <<std::endl; //or can be done by: table["in"]
+    // //Acessing:
+    // std::cout<< table.at("in") <<std::endl; // at("Key_name") returns what is present in that key.
+    // //Updating value using at:
+    // table.at("in") = "India2";
+    // std::cout<< table.at("in") <<std::endl;
+    // //Updating using []:
+    // table["in"] = "India3";
+    // std::cout<< table.at("in") <<std::endl; //or can be done by: table["in"]
 
-    std::unordered_map<std::string, std::string>::iterator it = table.begin();
-    std::cout << "Traversing the Unordered Map: ";
-    while(it != table.end()){
-        // std::cout << *it << " "; Here, our iterator is pointing that entry where key value pair is there. To print that K-V pair, we have to specify:
-        std::pair<std::string, std::string> p = *it;
-        std::cout << (*it).first << " " << (*it).second << std::endl; //Now this is pointing the first entry and increasing the "it" value afterwards, and firs is pointing the key, second is printing the value.
-        it++;
+    // std::unordered_map<std::string, std::string>::iterator it = table.begin();
+    // std::cout << "Traversing the Unordered Map: ";
+    // while(it != table.end()){
+    //     // std::cout << *it << " "; Here, our iterator is pointing that entry where key value pair is there. To print that K-V pair, we have to specify:
+    //     std::pair<std::string, std::string> p = *it;
+    //     std::cout << (*it).first << " " << (*it).second << std::endl; //Now this is pointing the first entry and increasing the "it" value afterwards, and firs is pointing the key, second is printing the value.
+    //     it++;
+    // }
+
+    //Erase:
+    // table.erase(table.begin(), table.end());
+
+    //find(key): if found, iterator of that location will be returned. If not found, it goes to end iterator (means returns it).
+    if (table.find("in")!= table.end()){
+        std::cout << "FOUND..!!";
     }
+    else{
+        std::cout << "NAHI MILI..!!";
+    }
+
+   //Count() -> Returns the number of element specified by a key; therefore, can be 0 or 1 only, as key can't be duplicated, so, either a key exists, or it does not.
+   if(table.count("in") == 0){
+    std::cout << "Key doesn't exist";
+   }
+   if(table.count("in") == 1){
+    std::cout << "Found";
+   }
+
 
     return 0;
 }
