01ee933c28c4325f037a0ff165653b7027b558ab
taniya-cyber <taniyaarora809@gmail.com>
2026-07-15 20:57:23 +0530

    Added the meaning of Algorithms in STL

--- STANDARD_TEMPLATE_LIBRARY/algorithms_stl/iterators&iterating_algos.cpp
+++ STANDARD_TEMPLATE_LIBRARY/algorithms_stl/iterators&iterating_algos.cpp
@@ -1,5 +1,7 @@
 #include<iostream>
 
+//Algorithms in STL: In STL, it includes a wide range of algorithms that operate on various containers (vectors, lists, sets and maps) and provide essential functionality for data manipulation. Tjese are defined in <algorithms> header and are part of STL's core functionality.
+
 int main(){
 
 
