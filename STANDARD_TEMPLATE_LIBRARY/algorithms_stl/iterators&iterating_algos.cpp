5ace7acfbabf3a85dc776d70cf746b74dc670ff5
taniya-cyber <taniyaarora809@gmail.com>
2026-07-20 15:29:37 +0530

    Algorithm: std::for_each

--- STANDARD_TEMPLATE_LIBRARY/algorithms_stl/iterators&iterating_algos.cpp
+++ STANDARD_TEMPLATE_LIBRARY/algorithms_stl/iterators&iterating_algos.cpp
@@ -1,12 +1,24 @@
-#include<iostream>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+// Algorithms in STL: In STL, it includes a wide range of algorithms that operate on various containers (vectors, lists, sets and maps) and provide essential functionality for data manipulation. Tjese are defined in <algorithms> header and are part of STL's core functionality.
 
-//Algorithms in STL: In STL, it includes a wide range of algorithms that operate on various containers (vectors, lists, sets and maps) and provide essential functionality for data manipulation. Tjese are defined in <algorithms> header and are part of STL's core functionality.
+void PrintDouble(int e){
+    std::cout << 2*e << std::endl;
+}
 
-int main(){
+int main()
+{
 
-
-
-
-
+    // 1. std::for_each: This algorithm is used when you nee to apply a specific function to a specific range, eg.:
+    std::vector<int> arr(4);
+    arr[0] = 1;
+    arr[1] = 2;
+    arr[2] = 3;
+    arr[3] = 4;
+    
+    //using for_each:
+    std::for_each(arr.begin(), arr.end(), PrintDouble);
+    
     return 0;
 }
