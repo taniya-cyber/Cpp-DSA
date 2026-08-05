978bde2411ad035ea8236159fa7d2f329eb02287
taniya-cyber <taniyaarora809@gmail.com>
2026-08-05 21:37:00 +0530

    Added lower bound searching algo

--- STANDARD_TEMPLATE_LIBRARY/algorithms_stl/searching&finding_algos.cpp
+++ STANDARD_TEMPLATE_LIBRARY/algorithms_stl/searching&finding_algos.cpp
@@ -4,22 +4,30 @@
 
 int main(){
 
-    std::vector<int> arr(5); 
-    //For applying Binary search, vector should be monotonic means in Order.
+    std::vector<int> arr; 
+    //1. For applying Binary search, vector should be monotonic means in Order.
     arr.push_back(10);
     arr.push_back(20);
     arr.push_back(30);
     arr.push_back(40);
     arr.push_back(50);
-    int target = 40;
-    std::binary_search(arr.begin(), arr.end(), target); //It returns BOOL value, if found, then 1, if not then 0. It doesn't Return an ITERATOR. 
 
-    if(std::binary_search(arr.begin(), arr.end(), target) == 0){
-        std::cout<< "NAHI EXIST KRTA";
-    }
-    else{
-        std::cout<< target << " FOunD";
-    }
+    // int target = 40;
+    // std::binary_search(arr.begin(), arr.end(), target); //It returns BOOL value, if found, then 1, if not then 0. It doesn't Return an ITERATOR. 
+
+    // if(std::binary_search(arr.begin(), arr.end(), target) == 0){
+    //     std::cout<< "NAHI EXIST KRTA";
+    // }
+    // else{
+    //     std::cout<< target << " FOunD";
+    // }
+
+    //2. std::lower_bound = Finds the first element greater or equal to a value in sorted range means:
+    auto IT1 = std::lower_bound(arr.begin(), arr.end(), 24); //As 24 is not in arr vector, so it will return Iterator for the greater value than or we can say just after 24, eg. 30.
+    std::cout<< *IT1 << std::endl;
+
+
+
 
 
 
