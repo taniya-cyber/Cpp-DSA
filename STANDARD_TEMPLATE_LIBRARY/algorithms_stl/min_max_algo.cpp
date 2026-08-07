bc814e59dbdc1728cae1a75a4e2acd597c0bc8e2
taniya-cyber <taniyaarora809@gmail.com>
2026-08-07 15:54:06 +0530

    Added min_element and max_element algos in function format

--- STANDARD_TEMPLATE_LIBRARY/algorithms_stl/min_max_algo.cpp
+++ STANDARD_TEMPLATE_LIBRARY/algorithms_stl/min_max_algo.cpp
@@ -3,10 +3,23 @@
 #include<vector>
 
 int main(){
-    //1. std::min = Returns the smaller of 2 values
-    int a = 22;
-    int b = 12;
-    std::cout<< std::min(a, b)<< std::endl;
-    std::cout<< std::max(a, b)<< std::endl;
+    // //1. std::min = Returns the smaller of 2 values
+    // int a = 22;
+    // int b = 12;
+    // std::cout<< std::min(a, b)<< std::endl;
+    // std::cout<< std::max(a, b)<< std::endl;
+
+    //2. std::min_element: Finds smallest in a range: and std::max_element is opposite
+    std::vector<int> arr;
+    arr.push_back(3);
+    arr.push_back(4);
+    arr.push_back(45);
+    arr.push_back(-34);
+    auto IT = std::min_element(arr.begin(), arr.end());
+    std::cout<< *IT << std::endl;
+
+    auto IT1 = std::max_element(arr.begin(), arr.end());
+    std::cout<< *IT1 << std::endl;
+    
     return 0; 
 }
