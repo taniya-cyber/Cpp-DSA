0cea583c0903e5682580c424a161798e26fa3064
taniya-cyber <taniyaarora809@gmail.com>
2026-08-15 18:31:33 +0530

    Added vector printing Iterator

--- STANDARD_TEMPLATE_LIBRARY/Iterators/Intro_to_Iteraros.cpp
+++ STANDARD_TEMPLATE_LIBRARY/Iterators/Intro_to_Iteraros.cpp
@@ -1,6 +1,9 @@
 #include<iostream>
-
+#include<vector>
 //Iterator: Is a pointer - like object representing an element's specific position in a container(can be vector, list, queue, etc.) and its used to iterate over elements in container, and we had pointers then still we made Iterators because its a standardized way for all containers to traverse on. Eg. container_name.begin(), container_name.end() and container_name.begin() + i takes you to ith index. 
+
+//Iterating on a vector:
+std::vector<int> ar;
 
 
 
