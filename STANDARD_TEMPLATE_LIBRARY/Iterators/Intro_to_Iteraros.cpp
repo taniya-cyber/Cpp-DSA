1cc0ee73554c1d834315f1032be73078cd86c5ef
taniya-cyber <taniyaarora809@gmail.com>
2026-08-19 18:55:15 +0530

    Added another Iterator Operation

--- STANDARD_TEMPLATE_LIBRARY/Iterators/Intro_to_Iteraros.cpp
+++ STANDARD_TEMPLATE_LIBRARY/Iterators/Intro_to_Iteraros.cpp
@@ -23,6 +23,9 @@ int main()
     //5. itr1 == itr2 means we are comparing them; if they are pointing same position, then true is returned
     //6. itr1 != itr2 means we are comparing them; if they are pointing DIFFERENT position, then true is returned
     //7. itr = itr1 = assigns/copies position by itr1 to itr iterator.
+    //8. itr->m = returns the member value m of object pointed by the iterator and is equivalent to (*itr).m, *itr represents A PAIR (pair<int, int>) inside a MAP, and pair has two types of values: pair.first(block 1), which can be accessed by (*itr).first or  itr->first and similalry for second. 
+    
+
     
     return 0;
 }
